5c20e7b609e59cccbd30b886781b6f61a8fe58a7
manhquynh <quynhtranza2@gmail.com>
2021-07-12 19:26:03 +0700

    them middlware

---
+++ app/Http/Middleware/checkAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class checkAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->user()->role_id !=3){
+            return route('home');
+        }
+        return $next($request);
+    }
+}
