d95bb6e079ed9ebffae475f95c3d9f2645841a03
manhquynh <quynhtranza2@gmail.com>
2021-07-15 17:20:25 +0700

    chinh route

--- routes/web.php
+++ routes/web.php
@@ -48,92 +48,26 @@ Auth::routes([
 //Question
 
 
-
-Route::post('/question/update', [QuestionsController::class, 'update'])->name('question.update');
-
-Route::post('/question/create', [QuestionsController::class, 'create'])->name('question.create');
-Route::post('/question/delete/{id}', [QuestionsController::class, 'delete'])->name('question.delete');
-Route::get('/questions', [QuestionsController::class, 'index'])->name('questions');
-Route::get('/question/detail/{id}', [QuestionsController::class, 'detail'])->name('question.detail');
-Route::get('/question/edit/{id}', [QuestionsController::class, 'edit'])->name('question.edit');
-
-Route::get('/question/add', [QuestionsController::class, 'add'])->name('question.add');
-
-
-
-//Add more answer
-Route::post('/answer/add/{id}', [ReliesController::class, 'add']);
-//Change correct answer
-Route::post('/answer/is_correct/{id}', [ReliesController::class, 'is_correct']);
-// delete answer
-Route::post('/answer/delete/{id}', [ReliesController::class, 'delete']);
-
-
-//staff
-Route::get('/staffs',[StaffsController::class, 'index'])->name('staff.index');
-Route::get('/staffs/show/{id}',[StaffsController::class, 'show'])->name('staff.show');
-Route::post('/staffs/update',[StaffsController::class,'update'])->name('staff.update');
-Route::post('/staffs/delete',[StaffsController::class,'drop'])->name('staff.drop');
-Route::post('/staffs/create',[StaffsController::class,'storge'])->name('staff.create');
-//staff
-Route::get('/branchs',[BranchsController::class, 'index'])->name('branch.index');
-Route::get('/branchs/show/{id}',[BranchsController::class, 'show'])->name('branch.show');
-Route::post('/branchs/update',[BranchsController::class,'update'])->name('branch.update');
-Route::post('/branchs/delete',[BranchsController::class,'drop'])->name('branch.drop');
-Route::post('/branchs/create',[BranchsController::class,'storge'])->name('branch.create');
-//contest
-Route::get('/home',[ContestsController::class,'home'])->name('home');
-Route::redirect('/', '/home' ); //redirect from / to /home when login in web
-
-
-
-//Contest
-Route::get('/contests', [ContestsController::class, 'index'])->name('contests');
-Route::get('/contest/add', [ContestsController::class, 'add'])->name('contest.add');
-Route::post('/contest/create', [ContestsController::class, 'create'])->name('contest.create');
-Route::get('/contest/edit/{id}', [ContestsController::class, 'edit'])->name('contest.edit');
-Route::post('/contest/update/{id}', [ContestsController::class, 'update'])->name('contest.update');
-Route::post('/contest/delete/{id}', [ContestsController::class, 'delete'])->name('contest.delete');
-Route::get('/contest/detail/{id}', [ContestsController::class, 'detail'])->name('contest.detail');
-
-
 //contest home direction
 Route::get('/home', [ContestsController::class, 'home'])->name('home');
 Route::redirect('/', '/home'); //redirect from / to /home when login in web
-// Exam
-Route::post('/contest/detail/{id}/exam/add', [ExamsController::class, 'add'])->name('exam.add');
 
-Route::post('/exam/init/{id}', function($id){
-    $procedure_name = 'THUCTAP.P_EXAMDETAIL';
-    $bindings = [
-        'v_exam_id' => $id,
-    ];
-    $init = DB::executeProcedure($procedure_name, $bindings);
 
-    // $procedure_name = 'THUCTAP.P_I_EXAMSTAFFS';
-    // $init = DB::executeProcedure($procedure_name, $bindings);
 
-    // $procedure_name = 'THUCTAP.P_I_EXAMQUE_REL';
-    // $init = DB::executeProcedure($procedure_name, $bindings);
-    return redirect()->back();
-    // dd($init);
-})->middleware('auth');
-
-Route::get('/exam/detail/{id}', [Examscontroller::class, 'detail'])->name('exam.detail');
-Route::get('/exam/edit/{id}', [Examscontroller::class, 'edit'])->name('exam.edit');
-//test
-Route::get('test03/{id}', [Examscontroller::class, 'test03'])->name('exam.test03');
-//exam_que_rels
-Route::get('test04', [ExamQueRelController::class, 'test04']);
-//test
-Route::get('testdethi',[test01::class,'testdethi']);
+// //test
+// Route::get('test03/{id}', [Examscontroller::class, 'test03'])->name('exam.test03');
+// //exam_que_rels
+// Route::get('test04', [ExamQueRelController::class, 'test04']);
+// //test
+// Route::get('testdethi', [test01::class, 'testdethi']);
 
 //check issuer make
 Route::middleware(checkIssuerMaker::class)->group(
     function () {
-        //questions
+
         Route::post('/question/update', [QuestionsController::class, 'update'])->name('question.update');
         Route::post('/question/create', [QuestionsController::class, 'create'])->name('question.create');
+        Route::post('/question/delete/{id}', [QuestionsController::class, 'delete'])->name('question.delete');
         Route::get('/questions', [QuestionsController::class, 'index'])->name('questions');
         Route::get('/question/detail/{id}', [QuestionsController::class, 'detail'])->name('question.detail');
         Route::get('/question/edit/{id}', [QuestionsController::class, 'edit'])->name('question.edit');
@@ -150,6 +84,29 @@ Route::middleware(checkIssuerMaker::class)->group(
         Route::post('/contest/update/{id}', [ContestsController::class, 'update'])->name('contest.update');
         Route::post('/contest/delete/{id}', [ContestsController::class, 'delete'])->name('contest.delete');
         Route::get('/contest/detail/{id}', [ContestsController::class, 'detail'])->name('contest.detail');
+        // Exam
+        Route::post('/contest/detail/{id}/exam/add', [ExamsController::class, 'add'])->name('exam.add');
+
+
+        Route::get('/exam/detail/{id}', [Examscontroller::class, 'detail'])->name('exam.detail');
+        Route::get('/exam/edit/{id}', [Examscontroller::class, 'edit'])->name('exam.edit');
+
+
+        Route::post('/exam/init/{id}', function ($id) {
+            $procedure_name = 'THUCTAP.P_EXAMDETAIL';
+            $bindings = [
+                'v_exam_id' => $id,
+            ];
+            $init = DB::executeProcedure($procedure_name, $bindings);
+
+            // $procedure_name = 'THUCTAP.P_I_EXAMSTAFFS';
+            // $init = DB::executeProcedure($procedure_name, $bindings);
+
+            // $procedure_name = 'THUCTAP.P_I_EXAMQUE_REL';
+            // $init = DB::executeProcedure($procedure_name, $bindings);
+            return redirect()->back();
+            // dd($init);
+        });
     }
 );
 
@@ -170,5 +127,3 @@ Route::middleware(checkAdmin::class)->group(
         Route::post('/branchs/create', [BranchsController::class, 'storge'])->name('branch.create');
     }
 );
-
-
