be1cd11b09e8b193bed88babf3082b38bb116c18
giahuy1999ct <72613819+giahuy1999ct@users.noreply.github.com>
2021-07-30 21:25:26 +0700

    change home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.default')
 
-
 @section('style')
     <link rel="stylesheet" href=" {{ asset('css/contests/home.css') }}">
 @endsection
@@ -14,62 +13,64 @@
         <br>
         <div class="row">
             @foreach ($contests as $contest)
-                <div class="col-md-1"></div>
-                <div class="container-ct col-md-5">
+                @foreach (App\Models\Exams::where('contest_id', $contest->contest_id)->get() as $exam)
+                    @if(App\Models\ExamStaffs::where('exam_id', $exam->id)->get()->count() !== 0)
+                        @php
+                            $able = true;
+                        @endphp
+                        @break
+                    @else
+                        @php
+                            $able = false;
+                        @endphp
+                    @endif
+                @endforeach
+                @if($able)
+                    <div class="col-md-1"></div>
+                    <div class="container-ct col-md-5">
 
-                    <div class="container__text">
-                        <h1>{{ $contest->contest->name }}</h1>
-                        <div class="container__text__timing">
-                            <div class="container__text__timing_time">
-                                <h2>mã dề thi </h2>
-                                <p>{{ $contest->contest->id }}</p>
+                        <div class="container__text">
+                            <h1>{{ $contest->contest->name }}</h1>
+                            <div class="container__text__timing">
+                                <div class="container__text__timing_time">
+                                    <h2>mã dề thi </h2>
+                                    <p>{{ $contest->contest->id }}</p>
+                                </div>
+                                <div class="container__text__timing_time">
+                                    <h2>ngày thi</h2>
+                                    <p> {{ date('d-m-Y H:i:s',$contest->contest->begintime_at) }}</p>
+                                </div>
+                                <div class="container__text__timing_time">
+                                    <h2>người tạo </h2>
+                                    <p>{{ $contest->contest->staff->name }}</p>
+                                </div>
                             </div>
-                            <div class="container__text__timing_time">
-                                <h2>ngày thi</h2>
-                                <p> {{ date('d-m-Y H:i:s',$contest->contest->begintime_at) }}</p>
+
+                            <p>
+                                {!! $contest->contest->content !!}
+                            </p>
+                            <div class="container__text__timing">
+
+                                <div class="container__text__timing_time">
+                                    <h2>ngày tạo</h2>
+                                    <p>{{ $contest->contest->created_at }}</p>
+                                </div>
+                                {{-- <div class="container__text__timing_time">
+                                    <h2>chi nhánh thi</h2>
+                                    <p>{{ $contest->contest->branch->name }}</p>
+                                </div> --}}
+
                             </div>
-                            <div class="container__text__timing_time">
-                                <h2>người tạo </h2>
-                                <p>{{ $contest->contest->staff->name }}</p>
-                            </div>
+                            <a class="btn" href="{{ route('exam.index', ['id' => $contest->contest->id]) }}"> xem các bài thi </a>
                         </div>
 
-                        <p>
-                            {!! $contest->contest->content !!}
-                        </p>
-                        <div class="container__text__timing">
-
-                            <div class="container__text__timing_time">
-                                <h2>ngày tạo</h2>
-                                <p>{{ $contest->contest->created_at }}</p>
-                            </div>
-                            {{-- <div class="container__text__timing_time">
-                                <h2>chi nhánh thi</h2>
-                                <p>{{ $contest->contest->branch->name }}</p>
-                            </div> --}}
-
-                        </div>
-                        <a class="btn" href="{{ route('exam.index', ['id' => $contest->contest->id]) }}"> xem các bài thi </a>
                     </div>
-
-                </div>
-
-
+                @endif
             @endforeach
 
         </div>
-
-
-
     </div>
-
-
-
-
 @endsection
 
 @section('js-content')
-
-
-
 @endsection
